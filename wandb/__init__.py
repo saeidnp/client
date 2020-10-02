8fbaf3dbff6adc580a65cbcbbaf669fadbc2354c
Fariz Rahman <farizrahman4u@gmail.com>
2020-10-02 16:33:16 +0530

    module level noop

--- wandb/__init__.py
+++ wandb/__init__.py
@@ -24,156 +24,175 @@ __version__ = '0.10.5.dev1'
 # Used with pypi checks and other messages related to pip
 _wandb_module = 'wandb'
 
+import os
 import sys
 
-from wandb.errors import Error
+if os.environ.get("WANDB_MODE") == "noop":
+    from .dummy import Dummy, DummyModule
+    dummy_wandb = DummyModule()
+    dummy_wandb.__version__ = __version__
+    sys.modules["wandb"] = dummy_wandb
+    class ImportHook:
+        def find_module(self, fullname, path=None):
+            if fullname.startswith(_wandb_module + '.'):
+                return self
 
-# This needs to be early as other modules call it.
-from wandb.errors.term import termlog, termerror, termwarn
+        def load_module(self, fullname):
+            module = DummyModule()
+            sys.modules[fullname] = module
+            return module
 
-PY3 = sys.version_info.major == 3 and sys.version_info.minor >= 6
-if PY3:
-    TYPE_CHECKING = True
-    from wandb import sdk as wandb_sdk
+    sys.meta_path.insert(0, ImportHook())
+
 else:
-    TYPE_CHECKING = False
-    from wandb import sdk_py27 as wandb_sdk
+    from wandb.errors import Error
 
-init = wandb_sdk.init
-setup = wandb_sdk.setup
-save = wandb_sdk.save
-watch = wandb_sdk.watch
-finish = wandb_sdk.finish
-join = finish
-login = wandb_sdk.login
-helper = wandb_sdk.helper
-Artifact = wandb_sdk.Artifact
-Settings = wandb_sdk.Settings
-Config = wandb_sdk.Config
+    # This needs to be early as other modules call it.
+    from wandb.errors.term import termlog, termerror, termwarn
 
-from wandb.apis import InternalApi, PublicApi
-from wandb.errors.error import CommError, UsageError
+    PY3 = sys.version_info.major == 3 and sys.version_info.minor >= 6
+    if PY3:
+        TYPE_CHECKING = True
+        from wandb import sdk as wandb_sdk
+    else:
+        TYPE_CHECKING = False
+        from wandb import sdk_py27 as wandb_sdk
 
-from wandb.lib import preinit as _preinit
-from wandb.lib import lazyloader as _lazyloader
-from wandb import wandb_torch
-from wandb import util
+    init = wandb_sdk.init
+    setup = wandb_sdk.setup
+    save = wandb_sdk.save
+    watch = wandb_sdk.watch
+    finish = wandb_sdk.finish
+    join = finish
+    login = wandb_sdk.login
+    helper = wandb_sdk.helper
+    Artifact = wandb_sdk.Artifact
+    Settings = wandb_sdk.Settings
+    Config = wandb_sdk.Config
 
-# Move this (keras.__init__ expects it at top level)
-from wandb.data_types import Graph
-from wandb.data_types import Image
-from wandb.data_types import Plotly
-from wandb.data_types import Video
-from wandb.data_types import Audio
-from wandb.data_types import Table
-from wandb.data_types import Html
-from wandb.data_types import Object3D
-from wandb.data_types import Molecule
-from wandb.data_types import Histogram
+    from wandb.apis import InternalApi, PublicApi
+    from wandb.errors.error import CommError, UsageError
 
-from wandb.wandb_agent import agent
-from wandb.wandb_controller import sweep, controller
+    from wandb.lib import preinit as _preinit
+    from wandb.lib import lazyloader as _lazyloader
+    from wandb import wandb_torch
+    from wandb import util
 
-from wandb import superagent
-from wandb.jupyteragent import jupyteragent as _secretagent
+    # Move this (keras.__init__ expects it at top level)
+    from wandb.data_types import Graph
+    from wandb.data_types import Image
+    from wandb.data_types import Plotly
+    from wandb.data_types import Video
+    from wandb.data_types import Audio
+    from wandb.data_types import Table
+    from wandb.data_types import Html
+    from wandb.data_types import Object3D
+    from wandb.data_types import Molecule
+    from wandb.data_types import Histogram
 
-# from wandb.core import *
-from wandb.viz import visualize
-from wandb import plots
-from wandb.integration.sagemaker import sagemaker_auth
+    from wandb.wandb_agent import agent
+    from wandb.wandb_controller import sweep, controller
 
+    from wandb import superagent
+    from wandb.jupyteragent import jupyteragent as _secretagent
 
-# Used to make sure we don't use some code in the incorrect process context
-_IS_INTERNAL_PROCESS = False
-
-
-def _set_internal_process():
-    global _IS_INTERNAL_PROCESS
-    _IS_INTERNAL_PROCESS = True
-
-
-def _is_internal_process():
-    return _IS_INTERNAL_PROCESS
+    # from wandb.core import *
+    from wandb.viz import visualize
+    from wandb import plots
+    from wandb.integration.sagemaker import sagemaker_auth
 
 
-from wandb.lib.ipython import _get_python_type
+    # Used to make sure we don't use some code in the incorrect process context
+    _IS_INTERNAL_PROCESS = False
 
-# toplevel:
-# save()
-# restore()
-# login()
-# sweep()
-# agent()
 
-# globals
-Api = PublicApi
-api = InternalApi()
-run = None
-config = _preinit.PreInitObject("wandb.config")
-summary = _preinit.PreInitObject("wandb.summary")
-log = _preinit.PreInitCallable(
-    "wandb.log", wandb_sdk.wandb_run.Run.log
-)
-save = _preinit.PreInitCallable(
-    "wandb.save", wandb_sdk.wandb_run.Run.save
-)
-restore = _preinit.PreInitCallable(
-    "wandb.restore", wandb_sdk.wandb_run.Run.restore
-)
-use_artifact = _preinit.PreInitCallable(
-    "wandb.use_artifact", wandb_sdk.wandb_run.Run.use_artifact
-)
-log_artifact = _preinit.PreInitCallable(
-    "wandb.log_artifact", wandb_sdk.wandb_run.Run.log_artifact
-)
-# record of patched libraries
-patched = {"tensorboard": [], "keras": [], "gym": []}
+    def _set_internal_process():
+        global _IS_INTERNAL_PROCESS
+        _IS_INTERNAL_PROCESS = True
 
-keras = _lazyloader.LazyLoader("wandb.keras", globals(), "wandb.integration.keras")
-sklearn = _lazyloader.LazyLoader("wandb.sklearn", globals(), "wandb.sklearn")
-tensorflow = _lazyloader.LazyLoader("wandb.tensorflow", globals(), "wandb.integration.tensorflow")
-xgboost = _lazyloader.LazyLoader("wandb.xgboost", globals(), "wandb.integration.xgboost")
-tensorboard = _lazyloader.LazyLoader("wandb.tensorboard", globals(), "wandb.integration.tensorboard")
-gym = _lazyloader.LazyLoader("wandb.gym", globals(), "wandb.integration.gym")
-lightgbm = _lazyloader.LazyLoader(
-    "wandb.lightgbm", globals(), "wandb.integration.lightgbm"
-)
-docker = _lazyloader.LazyLoader("wandb.docker", globals(), "wandb.docker")
-jupyter = _lazyloader.LazyLoader("wandb.jupyter", globals(), "wandb.jupyter")
-sacred = _lazyloader.LazyLoader("wandb.sacred", globals(), "wandb.integration.sacred")
 
-def ensure_configured():
-    global api
+    def _is_internal_process():
+        return _IS_INTERNAL_PROCESS
+
+
+    from wandb.lib.ipython import _get_python_type
+
+    # toplevel:
+    # save()
+    # restore()
+    # login()
+    # sweep()
+    # agent()
+
+    # globals
+    Api = PublicApi
     api = InternalApi()
+    run = None
+    config = _preinit.PreInitObject("wandb.config")
+    summary = _preinit.PreInitObject("wandb.summary")
+    log = _preinit.PreInitCallable(
+        "wandb.log", wandb_sdk.wandb_run.Run.log
+    )
+    save = _preinit.PreInitCallable(
+        "wandb.save", wandb_sdk.wandb_run.Run.save
+    )
+    restore = _preinit.PreInitCallable(
+        "wandb.restore", wandb_sdk.wandb_run.Run.restore
+    )
+    use_artifact = _preinit.PreInitCallable(
+        "wandb.use_artifact", wandb_sdk.wandb_run.Run.use_artifact
+    )
+    log_artifact = _preinit.PreInitCallable(
+        "wandb.log_artifact", wandb_sdk.wandb_run.Run.log_artifact
+    )
+    # record of patched libraries
+    patched = {"tensorboard": [], "keras": [], "gym": []}
+
+    keras = _lazyloader.LazyLoader("wandb.keras", globals(), "wandb.integration.keras")
+    sklearn = _lazyloader.LazyLoader("wandb.sklearn", globals(), "wandb.sklearn")
+    tensorflow = _lazyloader.LazyLoader("wandb.tensorflow", globals(), "wandb.integration.tensorflow")
+    xgboost = _lazyloader.LazyLoader("wandb.xgboost", globals(), "wandb.integration.xgboost")
+    tensorboard = _lazyloader.LazyLoader("wandb.tensorboard", globals(), "wandb.integration.tensorboard")
+    gym = _lazyloader.LazyLoader("wandb.gym", globals(), "wandb.integration.gym")
+    lightgbm = _lazyloader.LazyLoader(
+        "wandb.lightgbm", globals(), "wandb.integration.lightgbm"
+    )
+    docker = _lazyloader.LazyLoader("wandb.docker", globals(), "wandb.docker")
+    jupyter = _lazyloader.LazyLoader("wandb.jupyter", globals(), "wandb.jupyter")
+    sacred = _lazyloader.LazyLoader("wandb.sacred", globals(), "wandb.integration.sacred")
+
+    def ensure_configured():
+        global api
+        api = InternalApi()
 
 
-def set_trace():
-    import pdb  # TODO: support other debuggers
+    def set_trace():
+        import pdb  # TODO: support other debuggers
 
-    #  frame = sys._getframe().f_back
-    pdb.set_trace()  # TODO: pass the parent stack...
+        #  frame = sys._getframe().f_back
+        pdb.set_trace()  # TODO: pass the parent stack...
 
 
-__all__ = [
-    "__version__",
-    "init",
-    "setup",
-    "save",
-    "sweep",
-    "agent",
-    "config",
-    "log",
-    "summary",
-    "join",
-    "Api",
-    "Graph",
-    "Image",
-    "Plotly",
-    "Video",
-    "Audio",
-    "Table",
-    "Html",
-    "Object3D",
-    "Molecule",
-    "Histogram",
-]
+    __all__ = [
+        "__version__",
+        "init",
+        "setup",
+        "save",
+        "sweep",
+        "agent",
+        "config",
+        "log",
+        "summary",
+        "join",
+        "Api",
+        "Graph",
+        "Image",
+        "Plotly",
+        "Video",
+        "Audio",
+        "Table",
+        "Html",
+        "Object3D",
+        "Molecule",
+        "Histogram",
+    ]
---
+++ wandb/dummy.py
@@ -0,0 +1,45 @@
+from types import ModuleType
+
+
+class Dummy():
+    def __init__(self, *args, **kwargs):
+        binary_ops = ["add", "sub", "mul", "floordiv", "mod", "pow", "lshift", "rshift", "and", "xor", "or", "truediv"]
+        for op in binary_ops:
+            setattr(self, "__{}__".format(op), lambda *_: Dummy())
+        for i in range(len(binary_ops) - 1):
+            setattr(self, "__i{}__".format(binary_ops[i]), lambda *_: None)
+        self.__idiv__ = lambda *_: None
+        unary_ops = ["neg", "pos", "abs", "invert", "complex", "int", "long", "float", "oct", "hex"]
+        for op in unary_ops:
+            setattr(self, "__{}__".format(op), lambda : Dummy())
+        compare_ops = ["lt", "le", "eq", "ne", "gt", "ge"]
+        for op in compare_ops:
+            setattr(self, "__{}__".format(op), lambda _: True)
+        
+    def __getattr__(self, attr):
+        return Dummy()
+
+    def __getitem__(self, key):
+        return Dummy()
+
+    def __setitem__(self, key, value):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Dummy()
+
+    def __len__(self):
+        return 1
+
+    def __str__(self):
+        return ""
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        return exc_type is None
+
+
+class DummyModule(Dummy, ModuleType):
+    pass
